f8dad35e545b1022512fe11d1cbf1c669eae6aa0
vale <valerio.modugno@gmail.com>
2015-06-01 20:22:03 +0200

    safetycheck added

---
+++ Jaco1Control/safetycheck.hpp
@@ -0,0 +1,23 @@
+/*
+ * safetycheck.hpp
+ *
+ *  Created on: Jun 1, 2015
+ *      Author: vale
+ */
+
+#ifndef SAFETYCHECK_HPP_
+#define SAFETYCHECK_HPP_
+
+
+class safetycheck
+{
+
+
+
+
+};
+
+
+
+
+#endif /* SAFETYCHECK_HPP_ */
