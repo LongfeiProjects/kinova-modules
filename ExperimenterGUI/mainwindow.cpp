0c7f3ce7241df2c4b66ff351c6c9171ce3f9ff67
smarichal <sebarocker@gmail.com>
2015-12-02 19:21:40 +0100

    more functionality

--- ExperimenterGUI/mainwindow.cpp
+++ ExperimenterGUI/mainwindow.cpp
@@ -6,9 +6,103 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    initGUI();
+}
+
+void MainWindow::initGUI(){
+    this->ui->participantIdtextEdit->setDisabled(true);
+    this->ui->taskManagementGroupBox->setDisabled(true);
+
+    this->ui->taskscomboBox->addItem(tr("1:Turn on light"));
+    this->ui->taskscomboBox->addItem(tr("2:Push little switch"));
+    this->ui->taskscomboBox->addItem(tr("3:Push big switch"));
+    this->ui->taskscomboBox->addItem(tr("4:Open drawer"));
+    this->ui->taskscomboBox->addItem(tr("5:Pick and place object"));
+    this->ui->taskscomboBox->addItem(tr("6:Open door"));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::showErrorMessage(QString msg){
+    QMessageBox* msgBox = new QMessageBox(this);
+    msgBox->setWindowTitle(tr("Error"));
+    msgBox->setText(msg);
+    msgBox->exec();
+    delete msgBox;
+}
+
+void MainWindow::on_setButton_clicked()
+{
+    this->participantId = this->ui->participantIdSpinBox->value();
+    if(this->participantId>-1){
+           this->ui->participantIdtextEdit->setText(QString::number(this->participantId));
+           this->ui->participantIdIconFrame->setStyleSheet(QStringLiteral("image: url(:/img/ok.png);"));
+    }else{
+        showErrorMessage(tr("Invalid participant ID"));
+    }
+}
+
+
+bool MainWindow::checkExperimentPreConditions(){
+    bool res = false;
+    if(participantId>-1){
+        res=true;
+    }else{
+        showErrorMessage(tr("You must set the participant ID before start the experiment"));
+    }
+    return res;
+}
+
+void MainWindow::saveExperimentalData(){
+    //TODO
+    showErrorMessage(QString("Dumping data is still not implemented!! Do it!"));
+}
+
+void MainWindow::on_startExperimentButton_clicked()
+{
+    if(!this->experimentStarted){   //We have start the experiment
+        if(checkExperimentPreConditions()){
+            this->experimentStarted = true;
+            this->ui->taskManagementGroupBox->setDisabled(false);
+            time(&this->initExperimentTime);
+            this->ui->experimentStatusFrame->setStyleSheet(QStringLiteral("image: url(:/img/ok.png);"));
+            this->ui->startExperimentButton->setText(tr("Finish Experiment"));
+            this->ui->statustextEdit->setText(tr("STARTED!"));
+        }
+    }else{ //We have to finish the experiment
+        this->experimentStarted = false;
+        this->ui->experimentStatusFrame->setStyleSheet(QStringLiteral("image: url(:/img/wrong.png);"));
+        this->participantId = -1;
+        this->ui->participantIdSpinBox->setValue(-1);
+        this->ui->participantIdtextEdit->setText("");
+        this->ui->participantIdIconFrame->setStyleSheet(QStringLiteral("image: url(:/img/wrong.png);"));
+        this->ui->startExperimentButton->setText(tr("Start Experiment"));
+        this->ui->taskManagementGroupBox->setDisabled(true);
+        saveExperimentalData();
+    }
+}
+
+void MainWindow::on_startTask_clicked()
+{
+    if(!this->runningTask){
+        this->runningTask = true;
+        this->ui->startTask->setText(tr("Finish Task"));
+        time_t(&actualTask.initTimestamp);
+        actualTask.taskName = this->ui->taskscomboBox->currentText().toStdString();
+        this->ui->taskscomboBox->setDisabled(true);
+    }else{
+        time_t(&actualTask.endTimestamp);
+
+        //TODO Make a Dialog where the experimenter can add commets and also indicate if the task was finished properly
+
+        this->performedTasks.push_back(actualTask);
+        this->ui->listFinishedTasks->addItem(QString::fromStdString(actualTask.taskName));
+        this->ui->startTask->setText(tr("Start Task"));
+        this->ui->taskscomboBox->setDisabled(false);
+        this->runningTask=false;
+
+    }
 }
--- ExperimenterGUI/mainwindow.h
+++ ExperimenterGUI/mainwindow.h
@@ -1,11 +1,23 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <QMainWindow>
+#include <string.h>
+#include <vector>
+#include <qmainwindow.h>
+#include <qmessagebox.h>
+#include <time.h>
 
+using namespace std;
 namespace Ui {
 class MainWindow;
 }
+
+typedef struct{
+    time_t initTimestamp;
+    time_t endTimestamp;
+    string taskName;
+    bool finished;
+}Task;
 
 class MainWindow : public QMainWindow
 {
@@ -15,8 +27,33 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_setButton_clicked();
+
+    void on_startExperimentButton_clicked();
+
+    void on_startTask_clicked();
+
 private:
     Ui::MainWindow *ui;
+
+    int participantId = -1;
+    time_t initExperimentTime;
+    time_t endExperimentTime;
+    vector<Task> performedTasks;
+    Task actualTask;
+
+    bool experimentStarted = false;
+    bool runningTask = false;
+
+
+
+
+
+    void initGUI();
+    void showErrorMessage(QString msg);
+    bool checkExperimentPreConditions();
+    void saveExperimentalData();
 };
 
 #endif // MAINWINDOW_H
