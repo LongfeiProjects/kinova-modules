811503fdb01d5b162c08f480803dd0cf9e2d4687
vale <valerio.modugno@gmail.com>
2016-02-24 14:26:50 +0100

    probable fix to crash many trjaectories

--- Jaco1Control/src/devices/kinova_status_openAPI.cpp
+++ Jaco1Control/src/devices/kinova_status_openAPI.cpp
@@ -182,6 +182,9 @@ void kinova_status_openapi::StartSaving(std::vector<std::string>  & type){
 	}
     std::cout<<"starting saving"<<std::endl;
 	if(first_write.load(boost::memory_order_acquire)){
+		// stop the reading thread
+		this->running.store(false,boost::memory_order_release);
+		this->reader_stats->join();
 		for(unsigned int i =0;i<type.size();i++){
             DataStoreIt app;
 			if(type[i].compare("comp_t")==0){
@@ -211,12 +214,18 @@ void kinova_status_openapi::StartSaving(std::vector<std::string>  & type){
 			}
 			this->bookmarks[i].push_back(app);
 		}
+		// reactivate the reading thread
+		this->running.store(true,boost::memory_order_release);
+		this->reader_stats = new boost::thread(boost::bind(&kinova_status_openapi::Reading,this));
 		this->active_bookmarks.push_back(1);
 		this->already_saving = true;
 	 }
 }
 void kinova_status_openapi::SaveCheckPoint(std::vector<std::string>  & type){
 	if(already_saving){
+		 // stop the reading thread
+		this->running.store(false,boost::memory_order_release);
+		this->reader_stats->join();
 		for(unsigned int i =0;i<type.size();i++){
 			DataStoreIt app;
 			if(type[i].compare("comp_t")==0){
@@ -244,8 +253,12 @@ void kinova_status_openapi::SaveCheckPoint(std::vector<std::string>  & type){
 				app = this->ds_cart_pos.end();
 				app--;
 			}
+			// reactivate the reading thread
+			this->running.store(true,boost::memory_order_release);
+			this->reader_stats = new boost::thread(boost::bind(&kinova_status_openapi::Reading,this));
 			this->bookmarks[i].push_back(app);
 		}
+
 		this->active_bookmarks.push_back(1);
 	}
 }
